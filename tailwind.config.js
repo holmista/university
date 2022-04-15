e4e1e063a1ae7d39d11e641a4dfd572f61221d58
holmista <tornike.buchukuri.314@gmail.com>
2022-04-15 22:48:52 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Quicksand:"'Quicksand', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
