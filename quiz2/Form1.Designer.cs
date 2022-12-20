673d1d9d16dfef5768f7b15cac50ab8acf2c80d2
holmista <69036766+holmista@users.noreply.github.com>
2022-12-21 00:05:46 +0400

    quiz 2

---
+++ quiz2/Form1.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace quiz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FirstNameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.Label();
+            this.City = new System.Windows.Forms.Label();
+            this.Country = new System.Windows.Forms.Label();
+            this.Phone = new System.Windows.Forms.Label();
+            this.LastNameInput = new System.Windows.Forms.TextBox();
+            this.CityInput = new System.Windows.Forms.TextBox();
+            this.CountryInput = new System.Windows.Forms.TextBox();
+            this.PhoneInput = new System.Windows.Forms.TextBox();
+            this.createCustomerButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IdInput = new System.Windows.Forms.TextBox();
+            this.previewCustomer = new System.Windows.Forms.Button();
+            this.deleteCustomerButton = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.totalSpentTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.orderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.orderBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.unitPriceInput = new System.Windows.Forms.TextBox();
+            this.quantity = new System.Windows.Forms.Label();
+            this.quantityInput = new System.Windows.Forms.TextBox();
+            this.createOrderItemButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FirstNameInput
+            // 
+            this.FirstNameInput.Location = new System.Drawing.Point(120, 54);
+            this.FirstNameInput.Name = "FirstNameInput";
+            this.FirstNameInput.Size = new System.Drawing.Size(100, 20);
+            this.FirstNameInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "create or update a customer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(47, 57);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(54, 13);
+            this.FirstName.TabIndex = 2;
+            this.FirstName.Text = "FirstName";
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSize = true;
+            this.LastName.Location = new System.Drawing.Point(50, 94);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(55, 13);
+            this.LastName.TabIndex = 3;
+            this.LastName.Text = "LastName";
+            // 
+            // City
+            // 
+            this.City.AutoSize = true;
+            this.City.Location = new System.Drawing.Point(53, 123);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(24, 13);
+            this.City.TabIndex = 4;
+            this.City.Text = "City";
+            // 
+            // Country
+            // 
+            this.Country.AutoSize = true;
+            this.Country.Location = new System.Drawing.Point(53, 152);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(43, 13);
+            this.Country.TabIndex = 5;
+            this.Country.Text = "Country";
+            // 
+            // Phone
+            // 
+            this.Phone.AutoSize = true;
+            this.Phone.Location = new System.Drawing.Point(56, 182);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(38, 13);
+            this.Phone.TabIndex = 6;
+            this.Phone.Text = "Phone";
+            // 
+            // LastNameInput
+            // 
+            this.LastNameInput.Location = new System.Drawing.Point(120, 86);
+            this.LastNameInput.Name = "LastNameInput";
+            this.LastNameInput.Size = new System.Drawing.Size(100, 20);
+            this.LastNameInput.TabIndex = 7;
+            // 
+            // CityInput
+            // 
+            this.CityInput.Location = new System.Drawing.Point(120, 123);
+            this.CityInput.Name = "CityInput";
+            this.CityInput.Size = new System.Drawing.Size(100, 20);
+            this.CityInput.TabIndex = 8;
+            // 
+            // CountryInput
+            // 
+            this.CountryInput.Location = new System.Drawing.Point(120, 152);
+            this.CountryInput.Name = "CountryInput";
+            this.CountryInput.Size = new System.Drawing.Size(100, 20);
+            this.CountryInput.TabIndex = 9;
+            // 
+            // PhoneInput
+            // 
+            this.PhoneInput.Location = new System.Drawing.Point(120, 182);
+            this.PhoneInput.Name = "PhoneInput";
+            this.PhoneInput.Size = new System.Drawing.Size(100, 20);
+            this.PhoneInput.TabIndex = 10;
+            // 
+            // createCustomerButton
+            // 
+            this.createCustomerButton.Location = new System.Drawing.Point(88, 252);
+            this.createCustomerButton.Name = "createCustomerButton";
+            this.createCustomerButton.Size = new System.Drawing.Size(108, 23);
+            this.createCustomerButton.TabIndex = 11;
+            this.createCustomerButton.Text = "create or update";
+            this.createCustomerButton.UseVisualStyleBackColor = true;
+            this.createCustomerButton.Click += new System.EventHandler(this.createCustomerButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ID";
+            // 
+            // IdInput
+            // 
+            this.IdInput.Location = new System.Drawing.Point(120, 214);
+            this.IdInput.Name = "IdInput";
+            this.IdInput.Size = new System.Drawing.Size(100, 20);
+            this.IdInput.TabIndex = 13;
+            // 
+            // previewCustomer
+            // 
+            this.previewCustomer.Location = new System.Drawing.Point(88, 281);
+            this.previewCustomer.Name = "previewCustomer";
+            this.previewCustomer.Size = new System.Drawing.Size(108, 23);
+            this.previewCustomer.TabIndex = 14;
+            this.previewCustomer.Text = "preview customer";
+            this.previewCustomer.UseVisualStyleBackColor = true;
+            this.previewCustomer.Click += new System.EventHandler(this.previewCustomer_Click);
+            // 
+            // deleteCustomerButton
+            // 
+            this.deleteCustomerButton.Location = new System.Drawing.Point(88, 310);
+            this.deleteCustomerButton.Name = "deleteCustomerButton";
+            this.deleteCustomerButton.Size = new System.Drawing.Size(108, 23);
+            this.deleteCustomerButton.TabIndex = 15;
+            this.deleteCustomerButton.Text = "delete";
+            this.deleteCustomerButton.UseVisualStyleBackColor = true;
+            this.deleteCustomerButton.Click += new System.EventHandler(this.deleteCustomerButton_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(409, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(421, 150);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "total spent";
+            // 
+            // totalSpentTextBox
+            // 
+            this.totalSpentTextBox.Location = new System.Drawing.Point(472, 231);
+            this.totalSpentTextBox.Name = "totalSpentTextBox";
+            this.totalSpentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalSpentTextBox.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "create order item";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 397);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "order id";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(99, 397);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 438);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "product";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(97, 438);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(181, 21);
+            this.comboBox2.TabIndex = 23;
+            // 
+            // orderBindingSource
+            // 
+            this.orderBindingSource.DataSource = typeof(quiz2.Order);
+            // 
+            // orderBindingSource1
+            // 
+            this.orderBindingSource1.DataSource = typeof(quiz2.Order);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 472);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "unit price";
+            // 
+            // unitPriceInput
+            // 
+            this.unitPriceInput.Location = new System.Drawing.Point(97, 472);
+            this.unitPriceInput.Name = "unitPriceInput";
+            this.unitPriceInput.Size = new System.Drawing.Size(100, 20);
+            this.unitPriceInput.TabIndex = 25;
+            // 
+            // quantity
+            // 
+            this.quantity.AutoSize = true;
+            this.quantity.Location = new System.Drawing.Point(43, 503);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(44, 13);
+            this.quantity.TabIndex = 26;
+            this.quantity.Text = "quantity";
+            // 
+            // quantityInput
+            // 
+            this.quantityInput.Location = new System.Drawing.Point(97, 499);
+            this.quantityInput.Name = "quantityInput";
+            this.quantityInput.Size = new System.Drawing.Size(100, 20);
+            this.quantityInput.TabIndex = 27;
+            // 
+            // createOrderItemButton
+            // 
+            this.createOrderItemButton.Location = new System.Drawing.Point(56, 536);
+            this.createOrderItemButton.Name = "createOrderItemButton";
+            this.createOrderItemButton.Size = new System.Drawing.Size(75, 23);
+            this.createOrderItemButton.TabIndex = 28;
+            this.createOrderItemButton.Text = "create";
+            this.createOrderItemButton.UseVisualStyleBackColor = true;
+            this.createOrderItemButton.Click += new System.EventHandler(this.createOrderItemButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 599);
+            this.Controls.Add(this.createOrderItemButton);
+            this.Controls.Add(this.quantityInput);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.unitPriceInput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.totalSpentTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.deleteCustomerButton);
+            this.Controls.Add(this.previewCustomer);
+            this.Controls.Add(this.IdInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.createCustomerButton);
+            this.Controls.Add(this.PhoneInput);
+            this.Controls.Add(this.CountryInput);
+            this.Controls.Add(this.CityInput);
+            this.Controls.Add(this.LastNameInput);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FirstNameInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label LastName;
+        private System.Windows.Forms.Label City;
+        private System.Windows.Forms.Label Country;
+        private System.Windows.Forms.Label Phone;
+        private System.Windows.Forms.TextBox LastNameInput;
+        private System.Windows.Forms.TextBox CityInput;
+        private System.Windows.Forms.TextBox CountryInput;
+        private System.Windows.Forms.TextBox PhoneInput;
+        private System.Windows.Forms.Button createCustomerButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IdInput;
+        private System.Windows.Forms.Button previewCustomer;
+        private System.Windows.Forms.Button deleteCustomerButton;
+        private System.Windows.Forms.BindingSource orderBindingSource;
+        private System.Windows.Forms.BindingSource orderBindingSource1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox totalSpentTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox unitPriceInput;
+        private System.Windows.Forms.Label quantity;
+        private System.Windows.Forms.TextBox quantityInput;
+        private System.Windows.Forms.Button createOrderItemButton;
+    }
+}
+
---
+++ quiz2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (quiz2Entities context = new quiz2Entities())
+            {
+                Order[] orders = context.Orders.ToArray();
+                foreach (Order order in orders)
+                {
+                    comboBox1.Items.Add(order.Id);
+                }
+                var products = (from product in context.Products
+                                      select new {Value = product.Id, Key = product.ProductName}).ToList();
+                comboBox2.DataSource = products;
+                comboBox2.DisplayMember = "Key";
+                comboBox2.ValueMember = "Value";
+                
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createCustomerButton_Click(object sender, EventArgs e)
+        {
+            using (quiz2Entities context = new quiz2Entities())
+            {
+                if (IdInput.Text.Length > 0)
+                {
+                    int id = Int32.Parse(IdInput.Text);
+                    Customer existingCustomer = context.Customers.Where(c => c.Id == id).FirstOrDefault();
+                    if (existingCustomer != null)
+                    {
+                        existingCustomer.FirstName = FirstNameInput.Text;
+                        existingCustomer.LastName = LastNameInput.Text;
+                        existingCustomer.City = CityInput.Text;
+                        existingCustomer.Country = CountryInput.Text;
+                        existingCustomer.Phone = PhoneInput.Text;
+                    }
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Customer customer = new Customer()
+                    {
+                        FirstName = FirstNameInput.Text,
+                        LastName = LastNameInput.Text,
+                        City = CityInput.Text,
+                        Country = CountryInput.Text,
+                        Phone = PhoneInput.Text,
+                    };
+                    context.Customers.Add(customer);
+                    context.SaveChanges();
+                }
+                
+               
+            }
+        }
+
+        private void previewCustomer_Click(object sender, EventArgs e)
+        {
+            using (quiz2Entities context = new quiz2Entities())
+            {
+                if (IdInput.Text.Length > 0)
+                {
+                    int id = Int32.Parse(IdInput.Text);
+                    Customer existingCustomer = context.Customers.Where(c => c.Id == id).FirstOrDefault();
+                    if (existingCustomer != null)
+                    {
+                        FirstNameInput.Text = existingCustomer.FirstName;
+                        LastNameInput.Text = existingCustomer.LastName;
+                        CityInput.Text = existingCustomer.City;
+                        CountryInput.Text = existingCustomer.Country;
+                        PhoneInput.Text = existingCustomer.Phone;
+
+                        dataGridView1.ColumnCount = 4;
+                        dataGridView1.Columns[0].Name = "ProductName";
+                        dataGridView1.Columns[1].Name = "Total";
+                        dataGridView1.Columns[2].Name = "CompanyName";
+                        dataGridView1.Columns[3].Name = "OrderId";
+                        var CustomerOrders = (from item in context.OrderItems
+                                              join prod in context.Products
+                                              on item.ProductId equals prod.Id
+                                              join sup in context.Suppliers
+                                              on prod.SupplierId equals sup.Id
+                                              join order in context.Orders
+                                              on item.OrderId equals order.Id
+                                              where order.CustomerId == id
+                                              orderby order.Id ascending
+                                              select new { ProductName = prod.ProductName, Total = item.Quantity * item.UnitPrice, Supplier = sup.CompanyName, OrderId = order.Id }).ToList();
+                       
+                        foreach (var i in CustomerOrders)
+                        {
+                            dataGridView1.Rows.Add(i.ProductName, i.Total.ToString(), i.Supplier, i.OrderId);
+                        }
+                        var totalSpent = (from item in context.OrderItems
+                                          join order in context.Orders
+                                          on item.OrderId equals order.Id
+                                          where order.CustomerId == id
+                                          select item.Quantity*item.UnitPrice).Sum();
+                        totalSpentTextBox.Text = totalSpent.ToString();
+                    }
+                    
+                }
+            }
+               
+        }
+
+        private void deleteCustomerButton_Click(object sender, EventArgs e)
+        {
+            using (quiz2Entities context = new quiz2Entities())
+            {
+                if (IdInput.Text.Length > 0)
+                {
+                    int id = Int32.Parse(IdInput.Text);
+                    Customer existingCustomer = context.Customers.Where(c => c.Id == id).FirstOrDefault();
+                    if (existingCustomer != null)
+                    {
+                       context.Customers.Remove(existingCustomer);
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void createOrderItemButton_Click(object sender, EventArgs e)
+        {
+            using (quiz2Entities context = new quiz2Entities())
+            {
+                OrderItem item = new OrderItem()
+                {
+                    OrderId = Int32.Parse(comboBox1.Text),
+                    ProductId = (int)comboBox2.SelectedValue,
+                    UnitPrice = Int32.Parse(unitPriceInput.Text),
+                    Quantity = Int32.Parse(quantityInput.Text),
+                };
+                context.OrderItems.Add(item);
+                context.SaveChanges();
+            }
+        }
+    }
+}
